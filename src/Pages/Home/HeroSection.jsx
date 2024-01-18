9adfec690f9027ddc41851ca929bfb0fcde6ebf0
Mafalda Pires <mfpires111@gmail.com>
2024-01-18 15:52:10 +0000

    text in all page

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,24 +1,32 @@
+import React from 'react';
+
 export default function HeroSection() {
+  const openLinkedInProfile = () => {
+    // Replace 'YOUR_LINKEDIN_PROFILE_URL' with the actual URL of your LinkedIn profile
+    const linkedinProfileUrl = 'https://www.linkedin.com/in/mafalda-pires-b264ba1aa/';
+
+    // Open the LinkedIn profile URL in a new tab or window
+    window.open(linkedinProfileUrl, '_blank');
+  };
+
   return (
-    <section id ="heroSection" className="hero--section">
+    <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section-content">
           <p className="section--title">Hey, I'm Mafalda</p>
           <h1 className="hero--section--title">
-            <span className="hero--section--title--color">Full Stack</span>{" "}
-            <br />
-            Developer
+            <span className="hero--section--title--color">Full Stack</span> <br /> Developer
           </h1>
           <p className="hero--section-description">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            <br /> Libero error veniam illum cumque optio?
+            Looking forward to learn more each day <br /> and to bring <strong>fresh ideas</strong> to a dynamic team.
           </p>
         </div>
-        <button className="btn btn-primary">Get in Touch</button>
+        <button className="btn btn-primary" onClick={openLinkedInProfile}>
+          Get in Touch
+        </button>
       </div>
       <div className="hero--section--img">
-        <img src="./img/hero_img.png" alt="Hero Section" width={300}/>
+        <img src="./img/hero_img.png" alt="Hero Section" width={300} />
       </div>
     </section>
-  );
-}
+  );}
